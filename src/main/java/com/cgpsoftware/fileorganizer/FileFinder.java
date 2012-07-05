c549a3a72d3c15955ae1f539437d7e9949a4b235
cpall <christopher.pall@proquest.com>
2012-07-05 16:45:13 -0400

    Now produces a "pretend" set of output

--- src/main/java/com/cgpsoftware/fileorganizer/FileFinder.java
+++ src/main/java/com/cgpsoftware/fileorganizer/FileFinder.java
@@ -1,33 +1,56 @@
 package com.cgpsoftware.fileorganizer;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.AndFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
-
+/**
+ * Finds files in a directory using the apache tool.
+ * 
+ * Eventually, this could be tuned for very large datasets so that we didn't have to process the entire filelist 
+ * in memory. I imagine, since we are eventually going to store these file lists off in a database eventually, 
+ * we can just have multiple stages for filtering and processing:
+ * 
+ * 1. First stage scans the directories/files. (there could stage specific filters, based on filesystem information such as name, date, path)
+ * 2. Second stage scans the information from those files (there could be stage specific filters based on metadata within the files)
+ * 3. (processing stage)
+ * 4. Fourth stage could utilize information from any processing results and filter from there 
+ * (more processing or rinse and repeat 3rd and fourth steps -- technically, the second stage might be folded into the third, 
+ * even the first stage could get folded, but it might make sense to default the first and second to a typical usage scenario 
+ * instead of making everything abstract)
+ * 
+ * @author cpall
+ *
+ */
 public class FileFinder {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		File file = new File("F:/photo/Unorganized");
-		Collection<File> listFiles = FileUtils.listFiles(file, null, true);
+	public final File src;
+	public final List<IOFileFilter> fileFilters;
+	public final List<IOFileFilter> dirFilters;	
+	
+	public FileFinder(File src, List<IOFileFilter> fileFilters, List<IOFileFilter> dirFilters) {
+		this.src = src;
+		this.fileFilters = fileFilters;
+		this.dirFilters = dirFilters;
 		
+	}
+	
+	public List<FileInfo> find() {		
+		
+		Collection<File> listFiles = FileUtils.listFiles(src, new AndFileFilter(fileFilters), new AndFileFilter(dirFilters));
+		
+		List<FileInfo> fileList = new ArrayList<FileInfo>();		
 		for(File f:listFiles) {
-			String fileInfo;
-			
-			System.out.println(f);
+			FileInfo fileInfo = new FileInfo(f);
+			fileList.add(fileInfo);
 		}
-		System.out.println(listFiles.size());
+		return fileList;
+		
 	}
 
 }
--- src/main/java/com/cgpsoftware/fileorganizer/FileOrganizer.java
+++ src/main/java/com/cgpsoftware/fileorganizer/FileOrganizer.java
@@ -1,13 +1,52 @@
 package com.cgpsoftware.fileorganizer;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.cli.ParseException;
+
 public class FileOrganizer {
 
 	/**
 	 * @param args
+	 * @throws ParseException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException, IOException {
+		/**
+		 * TODO Generate an "Action" 
+		 * 
+		 */
+		FileOrganizerCLIConfig fileOrganizerCLIConfig = new FileOrganizerCLIConfig(args);
+		
+					
+		FileFinder fileFinder = new FileFinder(fileOrganizerCLIConfig.src, fileOrganizerCLIConfig.fileFilters, fileOrganizerCLIConfig.dirFilters);
+		List<FileInfo> foundFiles = fileFinder.find();
+		
+		for(FileInfo fileInfo:foundFiles) {
+			//System.out.println(fileInfo.getFile().getAbsolutePath());
+			
+			String dest = fileOrganizerCLIConfig.dest.getCanonicalPath();
+			
+			Map<String, String> contextMap = createContextMap(fileInfo);			
+			dest = dest + "/" + TemplateProcessor.process(contextMap, fileOrganizerCLIConfig.destSpec) + "/" + fileInfo.getFile().getName();
+						
+			//FileAction copyFileAction = new CopyFileAction(fileInfo.getFile(), new File(dest), null);
+			System.out.println(dest);
+		}
+		System.out.println(fileFinder.src + " has :" + foundFiles.size());
+		
+	}
 
+	private static Map<String, String> createContextMap(FileInfo fileInfo) {
+		Map<String, String> result = new HashMap<String, String>();			
+		result.put("year", (new SimpleDateFormat("yyyy")).format(fileInfo.getFile().lastModified()));
+		result.put("month", (new SimpleDateFormat("MM")).format(fileInfo.getFile().lastModified()));
+		result.put("day", (new SimpleDateFormat("dd")).format(fileInfo.getFile().lastModified()));
+		return result;
 	}
 
 }
